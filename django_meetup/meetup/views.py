c00683783270fd62400849b882b1f16b9a291bbf
eusoncode <eusoncode@gmail.com>
2025-02-10 21:17:00 -0700

    Added the Dummy data, rendered and styled it

--- django_meetup/meetup/views.py
+++ django_meetup/meetup/views.py
@@ -4,4 +4,19 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'meetup/index.html')
+    meetups = [
+        {
+            'title': 'A First Meetup',
+            'location': 'Edmonton soutwest',
+            'slug': 'a-first-meetup'
+        },
+        {
+            'title': 'A Second Meetup',
+            'location': 'Edmonton northwest',
+            'slug': 'a-second-meetup'
+        }
+    ]
+    return render(request, 'meetup/index.html', {
+        'meetups': meetups,
+        'show_meetups': True
+    })
