e5562e7da6a4120564a6929f706fce73d222eca8
eusoncode <eusoncode@gmail.com>
2025-02-11 22:45:21 -0700

    Added dynamic url in templates

--- django_meetup/meetup/urls.py
+++ django_meetup/meetup/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('meetup/', views.index), # our-domain.com/meetup
-  path('meetup/<slug:meetup_slug>', views.meetup_details) # our-domain.com/meetup/<dynamic-path-segment>
+  path('meetup/', views.index, name='all-meetups'), # our-domain.com/meetup
+  path('meetup/<slug:meetup_slug>', views.meetup_details, name='meetup-detail') # our-domain.com/meetup/<dynamic-path-segment>
 ]
