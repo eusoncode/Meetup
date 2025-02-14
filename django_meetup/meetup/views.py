e00c1220c0e2c0af696a6da4da837693addaee74
eusoncode <eusoncode@gmail.com>
2025-02-14 00:28:05 -0700

    Added form

--- django_meetup/meetup/views.py
+++ django_meetup/meetup/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Meetup
+from .forms import RegistrationForm
 
 # Create your views here.
 
@@ -34,9 +35,18 @@ def meetup_details(request, meetup_slug):
     """
     try:
         selected_meetup = Meetup.objects.get(slug=meetup_slug)
+        if request.method == 'GET':
+            registration_form = RegistrationForm()
+        else:
+            registration_form = RegistrationForm(request.POST)
+            if registration_form.is_valid():
+                participant = registration_form.save()
+                selected_meetup.participant.add(participant)
+        
         return render(request, 'meetup/meetup-details.html', {
             'meetup_found': True,
-            'meetup': selected_meetup
+            'meetup': selected_meetup,
+            'form': registration_form
         } )
     except Exception as exc:
         return render(request, 'meetup/meetup-details.html', {
