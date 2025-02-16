a6a9987cfa25330c339046300fa99069b4c3efe5
eusoncode <eusoncode@gmail.com>
2025-02-16 00:02:29 -0700

    Optimizing URLs

--- django_meetup/django_meetup/urls.py
+++ django_meetup/django_meetup/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('meetup.urls')) # our-domain.com/  -> point to meetup/........
+    path('', RedirectView.as_view(url='/meetup')), # our-domain.com/  -> point to meetup/........
+    path('meetup/', include('meetup.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- django_meetup/meetup/urls.py
+++ django_meetup/meetup/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('meetup/', views.index, name='all-meetups'), # our-domain.com/meetup
-  path('meetup/<slug:meetup_slug>/success', views.confirm_registration, name='confirm-registration'),
-  path('meetup/<slug:meetup_slug>', views.meetup_details, name='meetup-detail') # our-domain.com/meetup/<dynamic-path-segment>
+  path('', views.index, name='all-meetups'), # our-domain.com/meetup
+  path('<slug:meetup_slug>/success', views.confirm_registration, name='confirm-registration'),
+  path('<slug:meetup_slug>', views.meetup_details, name='meetup-detail') # our-domain.com/meetup/<dynamic-path-segment>
 ]
